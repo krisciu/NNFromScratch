f7c4dea70adbb66dbd5038e7f33b36f3b40b17e0
krisciu <kriscija@gmail.com>
2023-10-06 23:16:56 -0700

    add relu

--- src/functions.py
+++ src/functions.py
@@ -3,5 +3,8 @@ import pandas as pd
 import math
 
 
-def sigmoid(x):
+def sigmoid(x) -> float:
     return 1 / (1 + math.e ** (-x))
+
+def relu(x) -> float:
+    return max(x,0)
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,6 +1,6 @@
 import pytest
 import math
-from src.functions import sigmoid
+from src.functions import relu, sigmoid
 
 
 # test Sigmoid
@@ -12,3 +12,14 @@ def test_sigmoid_large_positive():
 
 def test_sigmoid_negative():
     assert sigmoid(-100) == pytest.approx(0.0, abs=1e-9)
+
+#Test ReLU
+def test_relu_zero():
+    assert relu(0) == 0
+
+def test_relu_negative():
+    assert relu(-10000.234234) == 0
+
+def test_relu_positive():
+    val = 10000.00234234
+    assert relu(val) == val
